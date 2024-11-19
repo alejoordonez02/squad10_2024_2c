fcb0dedc57108ac64d91de4937a621bf3c726d60
Alejo Ordoñez <alordonez@fi.uba.ar>
2024-11-19 03:12:03 -0300

    Inicializar el proyecto Spring Boot

---
+++ src/test/java/com/carga_horaria/carga_horaria/CargaHorariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carga_horaria.carga_horaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CargaHorariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
